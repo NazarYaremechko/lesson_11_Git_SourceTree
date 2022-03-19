bddce81d2a9b4bddab35a24072e4af560f7993e5
NazarYaremechko <naz.yaremechko@gmail.com>
2022-03-19 04:07:21 +0200

    added code from project-1-jsp-servlets-jdbc

---
+++ All_Implement_Project/src/main/java/implementProject_Complete/servlet/BucketController.java
@@ -0,0 +1,47 @@
+package implementProject_Complete.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import implementProject_Complete.domain.Bucket;
+import implementProject_Complete.service.BucketService;
+import implementProject_Complete.service.impl.BucketServiceImpl;
+
+@WebServlet("/bucket")
+public class BucketController extends HttpServlet {
+
+	private BucketService bucketService = BucketServiceImpl.getBucketService();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String productId = request.getParameter("productId");
+
+		HttpSession session = request.getSession();
+		Integer userId = (Integer) session.getAttribute("userId");
+
+		Bucket bucket = new Bucket(userId, Integer.parseInt(productId), new Date());
+		bucketService.create(bucket);
+
+		response.setContentType("text");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("Success");
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String bucketId = request.getParameter("bucketId");
+		bucketService.delete(Integer.parseInt(bucketId));
+
+		response.setContentType("text");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("Success");
+	}
+
+}
